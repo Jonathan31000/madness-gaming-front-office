ec251b83c19161facfbd9e5c3eea6666be7c1da1
Stroke9 <raoul.clement65@gmail.com>
2022-10-31 19:10:41 +0100

    computer function morpion

--- src/pages/morpion/index.js
+++ src/pages/morpion/index.js
@@ -12,37 +12,80 @@ document.addEventListener("DOMContentLoaded", () => {
   })
 })
 
-//tab = ["1-2", "2-1", ...];
+let Morpion = {
+  user: [],
+  computer: [],
+  drawUserCase: function (id) {
+    b(id).style.backgroundColor = "black";
+    Morpion.user.push(id);
+    if(!checkWin()) drawComputerCase();
+  },
+}
+window.Morpion = Morpion;
 
-function checkWin(){
+function drawComputerCase() {
+  let randomCase = "";
+  let availablePattern;
+  winPatern.map(elem => {
+    availablePattern = winPatern.filter(el => !Morpion.user.includes(elem));
+  })
+  console.log(availablePattern);
+  console.log(availablePattern.length);
+  b(randomCase).style.backgroundColor = "blue";
+  Morpion.computer.push(randomCase);
+}
+
+const winPatern = [
+  ["0-0", "0-1", "0-2"],
+  ["1-0", "1-1", "1-2"],
+  ["2-0", "2-1", "2-2"],
+  ["0-0", "1-0", "2-0"],
+  ["0-1", "1-1", "2-1"],
+  ["0-2", "1-2", "2-2"],
+  ["0-0", "1-1", "2-2"],
+  ["0-2", "1-1", "2-0"]
+]
+
+function checkWin() {
+  let isWin = false;
   winPatern.map(el => {
     let win = 0;
     el.map(elem => {
-      if(Morpion.player1.indexOf(elem) != -1) win++;
-      if(win == 3) return console.log('WIN!!');
+      if (Morpion.user.indexOf(elem) != -1) win++;
+      if (win == 3) isWin = true;
     })
   })
-}
-const winPatern = [
-  ["0-0", "0-1", "0-2"],
-  ["1-0","1-1","1-2"],
-  ["2-0","2-1","2-2"],
-  ["0-0","1-0","2-0"],
-  ["0-1", "1-1", "2-1"],
-  ["0-2", "1-2", "2-2"],
-  ["0-0","1-1","2-2"],
-  ["0-2","1-1","2-0"]
-]
-
-
-let Morpion = {
-  player1 : [],
-  rowDraw : function(id){
-    b(id).style.backgroundColor = "black";
-    Morpion.player1.push(id);
-    checkWin();
-  }
+  return isWin;
 }
 
+// function winPattern2(){
+//   let winPatternTab = [];
+//   let row = -1;
+//   let arrayDiag = [];
+//   let maxCase = 2;
+//   while (++row <= maxCase) {
+//     let col = -1;
+//     let arrayRow= [];
+//     let arrayCol= [];
+//     while (++col <= maxCase) {
+//       //["0-0","1-1","2-2"],
+//       //["0-2", "1-1", "2-0"]
+//       arrayRow.push(`${row}-${col}`);
+//       arrayCol.push(`${col}-${row}`);
+//       if(row == col) {
+//         arrayDiag.push(`${col}-${row}`);
+//       }
 
-window.Morpion = Morpion;
+//       if(row == maxCase) {
+//         arrayDiag.push(`${col}-${maxCase}`);
+//       }
+
+//     }
+//     winPatternTab.push(arrayRow);
+//     winPatternTab.push(arrayCol);
+//   }
+//   winPatternTab.push(arrayDiag);
+//   return winPatternTab;
+// }
+
+// console.log(winPattern2());
